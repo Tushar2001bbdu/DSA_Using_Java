94163f09f4ff1dc5dd057c62ed81abf8d36c32b7
Tushar2001bbdu <tusharkumaargupta032@gmail.com>
2024-09-12 23:33:08 +0530

    added leetcode-122

---
+++ Dynamic_Programming/BestTimeToBuyAndSellStock2.java
@@ -0,0 +1,80 @@
+/*
+ You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Total profit is 4.
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 3 * 10^4
+0 <= prices[i] <= 10^4
+ */
+
+ class Solution {
+    int[][] dp;
+     public int maxProfit(int[] prices) {
+        
+     dp=new int[prices.length+1][2];
+    
+     return solveTab(prices,dp);    
+     }
+     public int solveTab(int[] nums,int[][] dp){
+      for(int i=nums.length-1;i>=0;i--){
+          for(int j=1;j>=0;j--){
+           int p1=0,p2=0,p3=0,p4=0;
+         if(j==1){
+             p1=-nums[i]+dp[i+1][0];
+             p2=0+dp[i+1][j];
+         }
+         else{
+         p3=nums[i]+dp[i+1][1];
+         p4=0+dp[i+1][j];
+         }
+         dp[i][j]=(int)(Math.max(Math.max(p1,p2),Math.max(p3,p4)));   
+          }
+      }
+     return dp[0][1];
+     }
+     public int solveMem(int[] nums,int buy,int i){
+         if(i>=nums.length){
+             return 0;
+         }
+         if(dp[i][buy]!=-1){
+             return dp[i][buy];
+         }
+         int p1=0,p2=0,p3=0,p4=0;
+         if(buy==1){
+             p1=-nums[i]+solveMem(nums,0,i+1);
+             p2=0+solveMem(nums,1,i+1);
+         }
+         else{
+         p3=nums[i]+solveMem(nums,1,i+1);
+         p4=0+solveMem(nums,buy,i+1);
+         }
+         dp[i][buy]=(int)(Math.max(Math.max(p1,p2),Math.max(p3,p4)));
+         return dp[i][buy];
+     }
+ }
--- Dynamic_Programming/Maximal_Square.java
+++ Dynamic_Programming/Maximal_Square.java
@@ -30,3 +30,73 @@ m == matrix.length
 n == matrix[i].length
 1 <= m, n <= 300
 matrix[i][j] is '0' or '1'. */
+class Solution {
+    int[][] dp;
+    int max=Integer.MIN_VALUE;
+    public int maximalSquare(char[][] matrix) {
+     dp=new int[matrix.length][matrix[0].length];
+    for(int i=0;i<matrix.length;i++){
+        for(int j=0;j<matrix[0].length;j++){
+            dp[i][j]=-1;
+        }
+    }
+        int ans=solveTab(matrix);
+        return (ans*ans);
+    }
+    public int solveTab(char[][] mat){
+        int[] prev=new int[mat[0].length+1];
+        int[] curr=new int[mat[0].length+1];
+        int max=Integer.MIN_VALUE;
+        
+            for(int j=0;j<prev.length;j++){
+            prev[j]=0;
+            curr[j]=0;
+            }
+        
+        
+            for(int k=mat.length-1;k>=0;k--){
+            for(int j=mat[0].length-1;j>=0;j--){
+        int right=curr[j+1];
+         int down=prev[j];
+         int diagonal=prev[j+1];
+           if(mat[k][j] == '0'){
+            curr[j]=0;
+            max=(int)Math.max(curr[j],max);
+    
+        }
+        else{
+           curr[j]=1+(int)Math.min(right,Math.min(down,diagonal));
+            max=(int)Math.max(curr[j],max);    
+        }
+         
+            }
+                prev=curr;
+                curr=new int[mat[0].length+1];
+                
+            }
+        return max;
+        }
+        
+    
+    public int solve(char[][] mat,int i,int j){
+        if(i>=mat.length || j>=mat[0].length){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int right=solve(mat,i,j+1);
+         int down=solve(mat,i+1,j);
+         int diagonal=solve(mat,i+1,j+1);
+        if(mat[i][j]=='0'){
+            dp[i][j]=0;
+            max=(int)Math.max(dp[i][j],max);
+            return dp[i][j];
+        }
+        
+            dp[i][j]=1+(int)Math.min(right,Math.min(down,diagonal));
+            max=(int)Math.max(dp[i][j],max);
+            return dp[i][j];
+        }
+    
+}
