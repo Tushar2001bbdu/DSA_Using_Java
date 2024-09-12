12c85574be5251a0a900b6343b70be922f15cfcc
Tushar2001bbdu <tusharkumaargupta032@gmail.com>
2024-09-13 01:52:43 +0530

    added question

---
+++ Trees/MirrorTree.java
@@ -0,0 +1,190 @@
+/*
+ Given a Binary Tree, convert it into its mirror.
+MirrorTree1            
+
+Examples:
+
+Input:
+      1
+    /  \
+   2    3
+Output: 3 1 2
+Explanation: The tree is
+   1    (mirror)  1
+ /  \    =>      /  \
+2    3          3    2
+The inorder of mirror is 3 1 2
+Input:
+      10
+     /  \
+    20   30
+   /  \
+  40  60
+Output: 30 10 60 20 40
+Explanation: The tree is
+      10               10
+    /    \  (mirror) /    \
+   20    30    =>   30    20
+  /  \                   /   \
+ 40  60                 60   40
+The inroder traversal of mirror is
+30 10 60 20 40.
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 ≤ Number of nodes ≤ 105
+1 ≤ Data of a node ≤ 105
+
+
+ */
+//{ Driver Code Starts
+// Initial Template for Java
+
+// Contributed by Sudarshan Sharma
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+    void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+
+        inOrder(node.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+
+            Solution g = new Solution();
+            g.mirror(root);
+            GfG mt = new GfG();
+            mt.inOrder(root);
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+// function Template for Java
+
+// FUNCTION CODE
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        // Your code here
+        Queue<Node> que=new LinkedList<>();
+        que.add(node);
+        while(!(que.isEmpty())){
+            int le=que.size();
+            for(int i=1;i<=le;i++){
+                Node curr=que.poll();
+                Node temp=curr.left;
+                curr.left=curr.right;
+                curr.right=temp;
+                if(curr.left!=null){
+                    que.add(curr.left);
+                }
+                if(curr.right!=null){
+                    que.add(curr.right);
+                }
+            }
+        }
+    }
+}
